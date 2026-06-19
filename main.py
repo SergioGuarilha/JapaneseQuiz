e079df9ae1a8e8d7b9812882c312e9228ed0eaaf
SergioGuarilha <sergioguarilha@outlook.com>
2026-06-19 15:53:25 -0300

    add menu/deco cache system

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import pygame
-from menu import draw_menu, menu_rects
+from menu import draw_menu, menu_rects, config_rect
 from quizbuilder import draw_quiz, draw_result, quiz_rects, return_rect, get_correct_rect_index
+from configscreen import draw_config, config_screen_rect
 from audios import menu_click, return_sound, correct_guess, wrong_guess
 
 if __name__ =='__main__':
     on_result_screen = False
     on_menu_screen = True
+    on_config_screen = False
+    add_deco = True
     current_quiz = None
 
-    draw_menu()
+    draw_menu(add_deco)
     pygame.display.flip()
 
     running = True
@@ -20,18 +23,27 @@ if __name__ =='__main__':
             elif event.type == pygame.MOUSEBUTTONDOWN and on_menu_screen:
                 mouse_pos = pygame.mouse.get_pos()
                 for i, rect in enumerate(menu_rects):
-                    if rect.collidepoint(mouse_pos):
+                    if rect.collidepoint(mouse_pos) and not on_config_screen:
                         pygame.mixer.Sound.play(menu_click)
                         draw_quiz(i)
                         current_quiz = i
                         pygame.display.flip()
                         on_menu_screen = False
+                if config_rect.collidepoint(mouse_pos):
+                    pygame.mixer.Sound.play(menu_click)
+                    draw_config()
+                    pygame.display.flip()
+                    on_config_screen = True
+                elif not config_screen_rect.collidepoint(mouse_pos) and on_config_screen:
+                    draw_menu()
+                    pygame.display.flip()
+                    on_config_screen = False
 
             elif event.type == pygame.MOUSEBUTTONDOWN and not on_result_screen:
                 mouse_pos = pygame.mouse.get_pos()
                 if return_rect.collidepoint(mouse_pos):
                     pygame.mixer.Sound.play(return_sound)
-                    draw_menu()
+                    draw_menu(add_deco)
                     pygame.display.flip()
                     on_menu_screen = True
                 for i, rect in enumerate(quiz_rects):
--- menu.py
+++ menu.py
@@ -13,6 +13,10 @@ config_cog = pygame.image.load('./assets/ConfigCog.png')
 
 quiz_options = ["Hiragana", "Hiragana\n Combos", "Katakana", "Katakana\n Combos"]
 
+#Global variable
+menu_cache = None
+
+#Rectangle configurations
 dist_from_top = 410
 rect_width = 180
 rect_height = 140
@@ -27,43 +31,57 @@ menu_rects = [
     pygame.Rect(740, dist_from_top, rect_width, rect_height)
 ]
 
-def draw_menu():
-    screen.fill("white")
-
-    #draws menu background decoration
-    for i in range(10):
+def draw_menu(redecorate=False):
+    global menu_cache
+    if redecorate:
+        menu_cache = None
+    if menu_cache is None:
+        # Create a surface to store the menu
         screen_width = pygame.display.get_window_size()[0]
         screen_height = pygame.display.get_window_size()[1]
-        line_pattern = ((i * 10) + 5) / 100
-        line_placement = screen_height * line_pattern
-        pygame.draw.line(screen, pygame.Color("white"),[0, line_placement],[1000, line_placement], 5)
-        for j in range(25):
-            if j % 2 == 0:
-                hira_or_kata = hiradict
-            else:
-                hira_or_kata = katadict
-            deco_symbol = font.render(hira_or_kata['symbols'].__getitem__(random.randrange(len(hira_or_kata['symbols']))), True, pygame.Color("black"))
-            symbol_x = (j / 24) * screen_width
-            symbol_y = line_placement - (deco_symbol.get_height() // 2)
-            screen.blit(deco_symbol, (symbol_x, symbol_y))
+        menu_cache = pygame.Surface((screen_width, screen_height))
+        menu_cache.fill("white")
 
-    pygame.draw.rect(screen, "black", main_rect)
-    pygame.draw.rect(screen, "grey", main_rect, border_width)
-    title = font.render("Japanese Quiz", True, "white")
-    title_rect = title.get_rect(center=main_rect.center)
-    screen.blit(title, title_rect)
+        # Draw background decoration on menu_cache
+        for i in range(10):
+            line_pattern = ((i * 10) + 5) / 100
+            line_placement = screen_height * line_pattern
+            pygame.draw.line(menu_cache, pygame.Color("white"), [0, line_placement], [1000, line_placement], 5)
+            for j in range(25):
+                if j % 2 == 0:
+                    hira_or_kata = hiradict
+                else:
+                    hira_or_kata = katadict
+                deco_symbol = font.render(
+                    hira_or_kata['symbols'].__getitem__(random.randrange(len(hira_or_kata['symbols']))), True,
+                    pygame.Color("black"))
+                symbol_x = (j / 24) * screen_width
+                symbol_y = line_placement - (deco_symbol.get_height() // 2)
+                menu_cache.blit(deco_symbol, (symbol_x, symbol_y))
 
-    pygame.draw.rect(screen, "white", config_rect)
-    pygame.draw.rect(screen, "grey", config_rect, 4)
-    config_cog_rect = config_cog.get_rect(center=config_rect.center)
-    screen.blit(config_cog, config_cog_rect)
+        # Draw main rectangle on menu_cache
+        pygame.draw.rect(menu_cache, "black", main_rect)
+        pygame.draw.rect(menu_cache, "grey", main_rect, border_width)
+        title = font.render("Japanese Quiz", True, "white")
+        title_rect = title.get_rect(center=main_rect.center)
+        menu_cache.blit(title, title_rect)
 
-    for i, rect in enumerate(menu_rects):
-        pygame.draw.rect(screen, "black", rect)
-        pygame.draw.rect(screen, "grey", rect, border_width)
-        text = font.render(quiz_options[i], True, (255, 255, 255))
-        text_rect = text.get_rect(center=rect.center)
-        screen.blit(text, text_rect)
+        # Draw config button on menu_cache
+        pygame.draw.rect(menu_cache, "white", config_rect)
+        pygame.draw.rect(menu_cache, "grey", config_rect, 4)
+        config_cog_rect = config_cog.get_rect(center=config_rect.center)
+        menu_cache.blit(config_cog, config_cog_rect)
+
+        # Draw menu buttons on menu_cache
+        for i, rect in enumerate(menu_rects):
+            pygame.draw.rect(menu_cache, "black", rect)
+            pygame.draw.rect(menu_cache, "grey", rect, border_width)
+            text = font.render(quiz_options[i], True, (255, 255, 255))
+            text_rect = text.get_rect(center=rect.center)
+            menu_cache.blit(text, text_rect)
+
+    screen = pygame.display.get_surface()
+    screen.blit(menu_cache, (0, 0))
 
     for i in range(len(all_rect_numbers)):
         all_rect_numbers.pop(0)
